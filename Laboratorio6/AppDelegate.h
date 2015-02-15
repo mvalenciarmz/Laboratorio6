54322a1adc20885e84411f7f977a3098a7fabaa4
marcos <marcos@marcoss-Mac-mini.local>
2015-02-15 15:40:14 -0800

    Initial Commit

---
+++ Laboratorio6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Laboratorio6
+//
+//  Created by marcos on 2/15/15.
+//  Copyright (c) 2015 marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Laboratorio6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Laboratorio6
+//
+//  Created by marcos on 2/15/15.
+//  Copyright (c) 2015 marcos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
